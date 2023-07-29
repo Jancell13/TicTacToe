a7c3424b0caed65cd8c3f2a41e0566e9a86fc25a
Jancell13 <jancell642@gmail.com>
2023-07-28 19:55:06 -0500

    Reformat

--- src/constants.js
+++ src/constants.js
@@ -1,7 +1,7 @@
 export const TURNS = {
-  x: "❌",
-  o: "⚪",
-};
+  x: '❌',
+  o: '⚪',
+}
 
 export const winner_combos = [
   [0, 1, 2],
@@ -12,4 +12,4 @@ export const winner_combos = [
   [2, 5, 8],
   [0, 4, 8],
   [2, 4, 6],
-];
+]
--- src/logic/board.js
+++ src/logic/board.js
@@ -1,18 +1,18 @@
-import { winner_combos } from "../constants";
+import { winner_combos } from '../constants'
 export const checkWinner = (boardToCheck) => {
   for (const combo of winner_combos) {
-    const [a, b, c] = combo;
+    const [a, b, c] = combo
     if (
       boardToCheck[a] &&
       boardToCheck[a] === boardToCheck[b] &&
       boardToCheck[a] === boardToCheck[c]
     ) {
-      return boardToCheck[a];
+      return boardToCheck[a]
     }
   }
-  return null;
-};
+  return null
+}
 
 export const checkEndGame = (newBoard) => {
-  return newBoard.every((Square) => Square != null);
-};
+  return newBoard.every((Square) => Square != null)
+}
